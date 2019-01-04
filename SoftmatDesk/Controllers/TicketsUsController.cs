908b6af70cf236daa4c57377d070ea5bf2d4b2ca
Johan Enrique Pinzon Rodriguez <johan.pinzon1989@gmail.com>
2019-01-04 12:19:28 -0500

    Se arreglo la falla en la vista de edicion de nivel de soport

--- SoftmatDesk/Controllers/TicketsUsController.cs
+++ SoftmatDesk/Controllers/TicketsUsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SoftmatDesk.Models.DB_Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace SoftmatDesk.Controllers
 {
     public class TicketsUsController : Controller
     {
+        private softmatdeskEntities db = new softmatdeskEntities();
+
         // GET: TicketsUs
         public ActionResult Index()
         {
